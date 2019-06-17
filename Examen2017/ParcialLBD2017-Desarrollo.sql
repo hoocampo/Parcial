e69232ff1f1f32f143e0fa0cc47d1211f7cabc75
hoocampo <hoocampo89@gmail.com>
2019-06-17 01:00:02 -0300

    Add Punto 4

--- Examen2017/ParcialLBD2017-Desarrollo.sql
+++ Examen2017/ParcialLBD2017-Desarrollo.sql
@@ -14,3 +14,32 @@ SELECT NULL, NULL, NULL, NULL, NULL, NULL, NULL, SUM(D.precio*D.cantidad)
 FROM Detalles D;
 
 SELECT * FROM VTotalVentas;
+
+-- Punto 4
+
+DROP TABLE IF EXISTS AuditoriaOfertas;
+CREATE TABLE IF NOT EXISTS AuditoriaOfertas (
+	idAuditoria INT NOT NULL AUTO_INCREMENT,
+	idOferta INT NOT NULL,
+	descuento FLOAT NOT NULL,
+    fechaInicio DATETIME NOT NULL,
+    fechaFin DATETIME NOT NULL,
+    cantidadMinima INT NOT NULL,
+    cantidadMaxima INT,
+    usuario VARCHAR(45) NOT NULL,
+    maquina VARCHAR(45) NOT NULL,
+    fecha DATE NOT NULL,
+    PRIMARY KEY(idAuditoria)
+) ENGINE=INNODB;
+
+DELIMITER //
+CREATE TRIGGER `Trigger_Insert_Ofertas`
+AFTER INSERT ON Ofertas FOR EACH ROW
+BEGIN
+	IF(NEW.descuento>=0.1) THEN
+		INSERT INTO AuditoriaOfertas
+        VALUES(DEFAULT,NEW.idOferta,NEW.descuento, NEW.fechaInicio, NEW.fechaFin, NEW.cantidadMinima, NEW.cantidadMaxima,
+        SUBSTRING_INDEX(USER(), '@', 1),SUBSTRING_INDEX(USER(), '@', -1), NOW());
+	END IF;
+END //
+DELIMITER ;
